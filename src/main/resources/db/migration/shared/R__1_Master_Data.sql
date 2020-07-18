60d5aeec43834588d8249eba109cd7d0912e7ad9
Mohamad Al Sayadi <mohamad.alsayadi@alefeducation.com>
2020-07-18 22:57:23 +0400

    Changes for the third-run

--- src/main/resources/db/migration/shared/R__1_Master_Data.sql
+++ src/main/resources/db/migration/shared/R__1_Master_Data.sql
@@ -11,24 +11,24 @@ INSERT INTO
 # INSERT statements for the First Run
 # Comment out for the Third Run
 ########################################################################################################################
-INSERT INTO
-    content (id, name, category_id)
-VALUES (1, 'Content 1.1', 1), (2, 'Content 1.2', 1),
-       (3, 'Content 2.1', 2), (4, 'Content 2.2', 2)
-ON DUPLICATE KEY UPDATE id=id, name=VALUES(`id`), category_id=VALUES(`category_id`);
+# INSERT INTO
+#     content (id, name, category_id)
+# VALUES (1, 'Content 1.1', 1), (2, 'Content 1.2', 1),
+#        (3, 'Content 2.1', 2), (4, 'Content 2.2', 2)
+# ON DUPLICATE KEY UPDATE id=id, name=VALUES(`id`), category_id=VALUES(`category_id`);
 ########################################################################################################################
 
 # INSERT statements for the Third Run
 # Uncomment for the Third Run
 ########################################################################################################################
-# INSERT INTO
-#     content_topics (id, name)
-#     VALUES (1, 'Topic 1'), (2, 'Topic 2')
-#     ON DUPLICATE KEY UPDATE id=id, name=VALUES(`name`);
-#
-# INSERT INTO
-#     content (id, name, category_id, topic_id)
-# VALUES (1, 'Content 1.1', 1, 1), (2, 'Content 1.2', 1, 2),
-#        (3, 'Content 2.1', 2, 2), (4, 'Content 2.2', 2, 1)
-# ON DUPLICATE KEY UPDATE id=id, name=VALUES(`name`), category_id=VALUES(`category_id`), topic_id=VALUES(`topic_id`);
+INSERT INTO
+    content_topics (id, name)
+    VALUES (1, 'Topic 1'), (2, 'Topic 2')
+    ON DUPLICATE KEY UPDATE id=id, name=VALUES(`name`);
+
+INSERT INTO
+    content (id, name, category_id, topic_id)
+VALUES (1, 'Content 1.1', 1, 1), (2, 'Content 1.2', 1, 2),
+       (3, 'Content 2.1', 2, 2), (4, 'Content 2.2', 2, 1)
+ON DUPLICATE KEY UPDATE id=id, name=VALUES(`name`), category_id=VALUES(`category_id`), topic_id=VALUES(`topic_id`);
 ########################################################################################################################
